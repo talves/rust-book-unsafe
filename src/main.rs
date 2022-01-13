678e91b5cd7b72cf042634a62243ad4373b61278
talves <tony@3alves.com>
2022-01-12 16:15:50 -0800

    Concurrency: remove un used

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::slice;
+
+fn main() {
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let (a, b) = split_at_mut(&mut v, 3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+        let len = slice.len();
+        let ptr = slice.as_mut_ptr();
+
+        assert!(mid <= len);
+
+        unsafe {
+            (
+                slice::from_raw_parts_mut(ptr, mid),
+                slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+            )
+        }
+    }
+    unsafe fn dangerous() {
+        println!("Inside the dangerous function")
+    }
+
+    unsafe {
+        dangerous();
+    }
+}
